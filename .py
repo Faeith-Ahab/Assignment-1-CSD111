74dce2d46660f8863bb24319f5622d71ffb943d9
Faeith-Ahab <143060249+Faeith-Ahab@users.noreply.github.com>
2023-09-08 23:33:46 +0800

    .py

---
+++ .py
@@ -0,0 +1,35 @@
+# Question one: Write a python program to display the sum of even numbers less than
+even_numbers_less_than_8= (2,4,6)
+sum_of_even_numbers = (2+4+6)
+print(sum_of_even_numbers)
+
+# Question two: Write a program in python to input 5 numbers from the keyboard and find their average
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+total_number = (a + b + c + d + e)
+print(total_number)
+average = (total_number)/5
+print(type(average))
+print("The average is; "+ str(average))
+
+# Question three: Write a python program to print the output of the following operations
+
+# -5 + 8 * 6
+result = (-5+8*6)
+print(result)
+
+# (55+9)%9
+result = (55+9)%9
+print(result)
+
+
+#20 + -3 * 5/8
+result = (20+-3*5/8)
+print(result)
+
+# 5 + 15/3 * 2-8%3
+result = (5+15/3*2-8%3)
+print(result)
